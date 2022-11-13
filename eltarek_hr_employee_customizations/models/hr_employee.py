9c026adc10fded655a0f780e783bb4b426d003ba
Abo El Fadl <fodaseken1997@gmail.com>
2022-11-13 13:20:46 +0200

    updated[Eltarek_HR]new hr

--- eltarek_hr_employee_customizations/models/hr_employee.py
+++ eltarek_hr_employee_customizations/models/hr_employee.py
@@ -11,6 +11,14 @@ class HrEmployee(models.Model):
     hr_code = fields.Char('Hr Code')
     arabic_name = fields.Char('Arabic Name')
 
+    @api.constrains('hr_code')
+    def unique_hr_code(self):
+        hr_code = self.env['hr.employee'].search([('id','!=',self.id)])
+        if hr_code:
+            for hr in hr_code:
+                if self.hr_code == hr.hr_code:
+                    raise ValidationError('HR Code Cannot Be Duplicated')
+
 
 
 

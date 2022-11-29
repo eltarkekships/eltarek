adbff8e349b60d3b669ca7d29595c28cd89ce2c0
Abo El Fadl <fodaseken1997@gmail.com>
2022-11-29 10:50:30 +0200

    [UPDATED]income tax

---
+++ eltarek_income_tax/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sure Income Tax Configurations',
+    'data': [
+
+        'data/salary_rules.xml',
+        'data/data.xml',
+        'views/income_tax_setting.xml',
+        'security/ir.model.access.csv',
+    ],
+    'author': 'Centione'
+    , 'depends': ['hr', 'hr_payroll','sure_hr_payroll_base']
+}

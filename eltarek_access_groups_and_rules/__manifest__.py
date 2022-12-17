b7ffaf9c159b0be7dff9b302494ff404681d5d5c
Abo El Fadl <fodaseken1997@gmail.com>
2022-12-17 17:58:07 +0200

    [UPDATED] security access

--- eltarek_access_groups_and_rules/__manifest__.py
+++ eltarek_access_groups_and_rules/__manifest__.py
@@ -4,8 +4,9 @@
 
         'views/groups.xml',
         'views/rules.xml',
+        'views/centione_delivery_request.xml',
         # 'security/ir.model.access.csv',
     ],
     'author': 'Centione'
-    , 'depends': ['hr', 'hr_payroll','base']
+    , 'depends': ['hr', 'hr_payroll','base','eltarek_delivery_request_generic']
 }
--- eltarek_access_groups_and_rules/models/hr_employee.py
+++ eltarek_access_groups_and_rules/models/hr_employee.py
@@ -7,16 +7,16 @@ from odoo.exceptions import UserError, ValidationError
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    # def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-    #     res = super(HrEmployee, self).fields_view_get(view_id=view_id, view_type=view_type,
-    #                                                   toolbar=toolbar, submenu=False)
-    #     group_id = self.env['res.users'].has_group('eltarek_access_groups_and_rules.group_show_employee_payroll_time_off_attend')
-    #     doc = etree.XML(res['arch'])
-    #     if not group_id:
-    #         if view_type == 'form':
-    #             nodes = doc.xpath("//form[@string='Employee']")
-    #             for node in nodes:
-    #                 node.set('create', '0')
-    #                 node.set('edit', '0')
-    #             res['arch'] = etree.tostring(doc)
-    #     return res
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        res = super(HrEmployee, self).fields_view_get(view_id=view_id, view_type=view_type,
+                                                      toolbar=toolbar, submenu=False)
+        group_id = self.env['res.users'].has_group('eltarek_access_groups_and_rules.group_show_employee_payroll_time_off_attend')
+        doc = etree.XML(res['arch'])
+        if not group_id:
+            if view_type == 'form':
+                nodes = doc.xpath("//form[@string='Employee']")
+                for node in nodes:
+                    node.set('create', '0')
+                    node.set('edit', '0')
+                res['arch'] = etree.tostring(doc)
+        return res
